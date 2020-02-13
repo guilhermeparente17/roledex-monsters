c0db89f181ea5390506adeb2aa1fc2503fe9551f
guilherme_parente17 <guilherme.parente17@outlook.com>
2020-02-12 23:41:08 -0300

    Updates

---
+++ precache-manifest.49e4e7d3e9df17f4c4e99bfa254f9a9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea629347e1cf2424e3c14110b0101a25",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "7a735b90c20904372f2e",
+    "url": "/monsters-roledex/static/css/main.648fdc49.chunk.css"
+  },
+  {
+    "revision": "8c131baeb8b18b4896a1",
+    "url": "/monsters-roledex/static/js/2.52961d8b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-roledex/static/js/2.52961d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a735b90c20904372f2e",
+    "url": "/monsters-roledex/static/js/main.35a65c0e.chunk.js"
+  },
+  {
+    "revision": "560cd8b1b7ad04a38930",
+    "url": "/monsters-roledex/static/js/runtime-main.d1a5c0dd.js"
+  }
+]);
